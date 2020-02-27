83239781476b6b26c6c534a53ae82953ae6a06bd
swarnaacharya <55823914+swarnaacharya@users.noreply.github.com>
2020-02-27 11:30:23 +0530

    webservice

---
+++ SpringCRUDwebservice/src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,54 @@
+package com.example.demo.model;
+
+public class Student {
+	private int sid;
+	private String  sname;
+	private String email;
+	
+	
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sname=" + sname + ", email=" + email + "]";
+	}
+
+
+	public int getSid() {
+		return sid;
+	}
+
+
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+
+
+	public String getSname() {
+		return sname;
+	}
+
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public Student(int sid, String sname, String email) {
+		super();
+		this.sid = sid;
+		this.sname = sname;
+		this.email = email;
+	}
+	
+	
+
+}
